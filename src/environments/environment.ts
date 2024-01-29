bb17481b4299a9973ac17302983f648856868d97
Isuri001 <isuriupeksha258@gmail.com>
2024-01-29 13:49:28 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDV8GbFCuZ7iG-OYRSVPFin6F7JKCPTydk",
+    authDomain: "hospital-management-syst-44d2c.firebaseapp.com",
+    projectId: "hospital-management-syst-44d2c",
+    storageBucket: "hospital-management-syst-44d2c.appspot.com",
+    messagingSenderId: "948017408174",
+    appId: "1:948017408174:web:973e5922083c3729b5a53f"
+  }
+};
+
+
